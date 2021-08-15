f70a8a9c29d634ef312cbb082649d3760eec21f2
Francisc <franciscboicu@gmail.com>
2021-08-15 21:09:54 +0300

    Basic methods

--- src/AbstractRepository.php
+++ src/AbstractRepository.php
@@ -16,21 +16,6 @@ Abstract class AbstractRepository
         return $this->getModel()->get();
     }
 
-    public function all()
-    {
-        return $this->getModel()->all();
-    }
-
-    public function delete($id)
-    {
-        return $this->getModel()->destroy($id);
-    }
-
-    public function update(array $data)
-    {
-        return $this->getModel()->update($data);
-    }
-
     public function find($id)
     {
         return $this->getModel()->find($id);
@@ -41,8 +26,56 @@ Abstract class AbstractRepository
         return $this->getModel()->findOrFail($id);
     }
 
+
+    public function all($columns = ['*'], $relations = [])
+    {
+        return $this->getModel()->with($relations)->get($columns);
+    }
+
+    public function allTrashed()
+    {
+        return $this->getModel()->onlyTrashed()->get();
+    }
+
+    public function findById($id, $columns=['*'], $relations=[], $append=[])
+    {
+        return $this->getModel()->select($columns)->with($relations)->findOrFail($id)->append($append);
+    }
+
+    public function findTrashedById($id)
+    {
+        return $this->getModel()->withTrashed()->findOrFail($id);
+    }
+
+    public function findOnlyTrashedById($id)
+    {
+        return $this->getModel()->onlyTrashed()->findOrFail($id);
+    }
+
     public function create($data)
     {
-        return $this->getModel()->create($data);
+        $newlyCreatedModel = $this->getModel()->create($data);
+
+        return $newlyCreatedModel->fresh();
+    }
+
+    public function update($id, $data)
+    {
+        return $this->findById($id)->update($data);
+    }
+
+    public function deleteById($id)
+    {
+        return $this->findById($id)->delete();
+    }
+
+    public function restoreById($id)
+    {
+        return $this->findOnlyTrashedById($id)->restore();
+    }
+
+    public function permanentDeleteById($id)
+    {
+        return $this->findTrashedById($id)->forceDelete();
     }
 }
